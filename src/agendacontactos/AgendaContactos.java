008a4c04564b88997109efb360e994a52119481e
Javier <Javier@LAPTOP-F8B321QU>
2018-05-10 10:20:06 +0200

    Crucigramo 10/05/18

--- src/agendacontactos/AgendaContactos.java
+++ src/agendacontactos/AgendaContactos.java
@@ -5,6 +5,9 @@
  */
 package agendacontactos;
 
+import BDEmpresa.Empleado;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -16,8 +19,28 @@ import javax.persistence.Persistence;
 public class AgendaContactos{
     
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AgendaContactosPU");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("EmpresaPU");
         EntityManager em = emf.createEntityManager();
+    
+        // Cerrar la conexión con la base de datos
+        em.close(); 
+        emf.close(); 
+        try { 
+            DriverManager.getConnection("jdbc:derby:BDEmpresa;shutdown=true"); 
+        } catch (SQLException ex) { 
+        }
+    
+        //PARA REALIZAR UNA SECUENCIA HABITUAL
+            //PARA INIICIAR UNA TRANSACCION
+            //em.getTransaction().begin();
+
+            //PARA REALIZAR UN VOLCADO 
+            //em.getTransaction().commit();
+        //
+        
+        em.getTransaction().begin();
+        Empleado empleadoCharllie = new Empleado() ;
+        em.getTransaction().commit();
     }
     
 }
